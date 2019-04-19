4505e6918f2ed69ae9743c3489f341805c47f9ed
PineGolden311 <software.developer112@gmail.com>
2019-04-19 07:05:47 +0400

    styling index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
+  resources :posts
+  post 'likes/:post_id', to: 'posts#add_like', as: 'add_likes'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
