7b024235f5c486fcce6169b1806f08def1cfb7ae
PineGolden311 <software.developer112@gmail.com>
2019-04-21 19:21:49 -0600

    update model relatives

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,8 @@
 class Like < ApplicationRecord
+  after_save :notify_pusher, on: :create
   belongs_to :post
+
+  def notify_pusher
+    Pusher.trigger('feed', 'new-like', self.post.as_json(include: :likes))
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  after_create :notify_pushder, on: :create
   has_many :likes
+
+  def notify_pushder
+    Pusher.trigger('feed', 'new-post', self.as_json(include: :likes))
+  end
 end
